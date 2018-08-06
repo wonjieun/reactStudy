dd57a55334c5ab29d14922cd2e52600e6cc4b340
wonjieun <wonjieunkk@gmail.com>
2018-08-07 07:32:07 +0900

    hello-react assignment1

---
+++ jieunWon/hello-react/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          Hello
+        </div>
+        <div>
+          Bye
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
