4f3d55b105b80d445f8ca89f599fa8488f625200
wonjieun <wonjieunkk@gmail.com>
2018-08-15 19:22:46 +0900

    default props

--- jieunWon/hello-react/src/App.js
+++ jieunWon/hello-react/src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
+import MyName from './MyName';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>
-          Hello
-        </div>
-        <div>
-          Bye
-        </div>
-      </div>
+      <MyName />
     );
   }
 }
