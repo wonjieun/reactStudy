edc638207c95b3b9499e91d3919555f6c904ff3e
wonjieun <wonjieunkk@gmail.com>
2018-08-16 09:08:45 +0900

    todo list first component

--- jieunWon/todo-list/src/App.js
+++ jieunWon/todo-list/src/App.js
@@ -1,47 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
-import Todo from './components/Todo';
+import TodoListTemplate from './components/TodoListTemplate';
 
 class App extends Component {
-  state = {
-    list:[
-      {
-        id: 0,
-        text: '안녕',
-        delete: false,
-        check: false
-      },
-      {
-        id: 1,
-        text: '테스트',
-        delete: false,
-        check: false
-      },
-      {}
-    ]
-  }
-
   render() {
-
-    const { list } = this.state;
-    const todolist = list.map(
-      list => ( <Todo key={list.id} info={list.text} /> )
-    );
-
     return (
-      <div className="todo-list-template">
-        <div className="title">
-          오늘 할 일
-        </div>
-        <div className="form-wrapper">
-          <div className="form">
-            <input type="text" /><button>추가</button>
-          </div>
-        </div>
-        <div className="todos-wrapper">
-          
-        </div>
-    </div>
+      <TodoListTemplate>
+        템플릿 완성
+      </TodoListTemplate>
     );
   }
 }
