03c471eb6939a81d0fca3867e865b56076047693
wonjieun <wonjieunkk@gmail.com>
2018-08-19 17:08:18 +0900

    8/16 studying...

--- jieunWon/todo-list/src/App.js
+++ jieunWon/todo-list/src/App.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
+  state = {
+    value: '테스트',
+    todo: []
+  }
+
+  updateState = (e) => {
+    // console.log(e.target);
+    this.setState({value: e.target.value});
+  }
+
+  updateKey = (e) => {
+    // console.log(e.target);
+    this.setState({value: e.target.value});
+  }
+
+  updateItem = (e) => {
+    // console.log(this.state.todo.length);
+    const { value, todo } = this.state;
+    this.setState({
+      value: '',
+      todo: todo.concat({
+        id: this.state.todo.length,
+        text: value
+      })
+    });
+  }
+
+  componentDidUpdate(){
+    console.log(this.state.todo[this.state.todo.length-1].text);
+  }
+
   render() {
     return (
-      <TodoListTemplate form={<Form/>}>
-        템플릿 완성
-      </TodoListTemplate>
+      <Form
+        value={this.state.value}
+        onChange={this.updateState}
+        onKeyPress={this.updateKey}
+        onCreate={this.updateItem}
+      />
+      // <TodoListTemplate form={<Form/>}>
+      //    <TodoItemList />
+      // </TodoListTemplate>
     );
   }
 }
