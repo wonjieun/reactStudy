81aca674a35f0e1467157ca43abf6f9284f38497
wonjieun <wonjieunkk@gmail.com>
2018-08-28 21:45:10 +0900

    08/28 props and state

--- jieunWon/hello-react/src/App.js
+++ jieunWon/hello-react/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import MyName from './MyName';
+import Counter from './Counter';
 
 class App extends Component {
   render() {
     return (
-      <MyName name="리액트" />
+      <Counter />
     );
   }
 }
--- jieunWon/hello-react/src/Counter.js
+++ jieunWon/hello-react/src/Counter.js
@@ -5,10 +5,31 @@ class Counter extends Component {
         number: 0
     }
 
+    // () =>
+    //버튼에서 클릭이벤트가 발생했을 때, this가 undefined으로 나타나는 것을 방지
     handleIncrease = () => {
         this.setState({
             number: this.state.number + 1
         });
+        /*
+        방법1
+        this.setState(
+            (state) => ({
+                number: state.number
+            })
+        );
+        방법2 (비구조화 할당...)
+        this.setState(
+            ({ number }) => ({
+                number: number + 1
+            })
+        );
+        방법3
+        const { number } = this.state;
+        this.setState({
+            number: number + 1
+        })
+        */
     }
 
     handleDecrease = () => {
@@ -17,11 +38,14 @@ class Counter extends Component {
         });
     }
 
+    //이벤트 설정을 한 부분
     render() {
         return (
             <div>
                 <h1>카운터</h1>
                 <div>값: {this.state.number}</div>
+                {/* <button onclick="alert('hello');">Click Me</button> */}
+                {/* 1. camelCase 2. 메소드를 호출하지 않기 ()X */}
                 <button onClick={this.handleIncrease}>+</button>
                 <button onClick={this.handleDecrease}>-</button>
             </div>
